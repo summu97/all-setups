ba0ce5e016ed81e55b00e0df73dea0acccbbd8b7
summu97 <135579543+summu97@users.noreply.github.com>
2023-12-14 10:49:17 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,15 +1,10 @@
-# SETUP:run all the commands one by one
+amazon-linux-extras install java-openjdk11 -y
+wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.83.tar.gz
+tar -zxvf apache-tomcat-9.0.83.tar.gz
+sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
+sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
+sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
 
-1:  wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80.tar.gz
-2:  tar -zxvf apache-tomcat-9.0.80.tar.gz && cd apache-tomcat-9.0.80
-3:  vim webapps/manager/META-INF/context.xml  (delete line 21 and 22)
-4:  vim conf/tomcat-users.xml (copy paste those lines in last)
-
-# <role rolename="manager-gui"/>
-# <role rolename="manager-script"/>
-# <user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>
-
-5:  ./bin/startup.sh
-
-In browser:
-login: public-ip:8080 -- > manager app -- > username: tomcat passowrd:raham123 -- > enter
+sed -i '21d' apache-tomcat-9.0.83/webapps/manager/META-INF/context.xml
+sed -i '22d'  apache-tomcat-9.0.83/webapps/manager/META-INF/context.xml
+sh apache-tomcat-9.0.83/bin/startup.sh
