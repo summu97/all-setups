ab46cdec58527591ed7d1cb910d90485056fcc07
summu97 <135579543+summu97@users.noreply.github.com>
2023-12-14 10:57:01 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -5,6 +5,7 @@ sed -i '56  a\<role rolename="manager-gui"/>' apache-tomcat-9.0.83/conf/tomcat-u
 sed -i '57  a\<role rolename="manager-script"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
 sed -i '58  a\<user username="tomcat" password="raham123" roles="manager-gui, manager-script"/>' apache-tomcat-9.0.83/conf/tomcat-users.xml
 sed -i '59  a\</tomcat-users>' apache-tomcat-9.0.83/conf/tomcat-users.xml
+sed -i '56d' apache-tomcat-9.0.83/conf/tomcat-users.xml
 sed -i '21d' apache-tomcat-9.0.83/webapps/manager/META-INF/context.xml
 sed -i '22d'  apache-tomcat-9.0.83/webapps/manager/META-INF/context.xml
 sh apache-tomcat-9.0.83/bin/startup.sh
